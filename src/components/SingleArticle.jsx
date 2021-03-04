116dd6f18649be6e01117183e672d62c4873575d
FoxyTony <mirantony@icloud.com>
2021-03-04 20:49:42 +0100

    defined the useEffect and started on the request function

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -4,6 +4,12 @@ import { useParams } from 'react-router-dom'
 const SingleArticle = () => {
   const { id } = useParams()
 
+  useEffect = () => {
+    const fetchData = async () => {
+      let response = await getSingleArticle(id)
+    }
+  }
+
   return (
     <div>
       HELLO FROM ARTICLE {id}
--- src/modules/articlesDataModule.js
+++ src/modules/articlesDataModule.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
+const getSingleArticle = async (id) => {
+  let response = await axios.get(`/articles/${id}`)
+}
+
 const getExploreArticles = async (articleType) => {
   let response = await axios.get(`/articles?article_type=${articleType}`)
   return response.data.articles
