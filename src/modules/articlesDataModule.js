69e0437b720b72802ab74b010417cf14a82aed86
Lucas Knudsen <laksteelhouse@gmail.com>
2021-03-04 15:37:17 +0100

    Continued merge

--- src/modules/articlesDataModule.js
+++ src/modules/articlesDataModule.js
@@ -1,11 +1,7 @@
 import axios from 'axios'
 
 const getExploreArticles = async (articleType) => {
-<<<<<<< HEAD
-  let response = await axios.get(`/api/articles?article_type=${articleType}`)
-=======
   let response = await axios.get(`/articles?article_type=${articleType}`)
->>>>>>> 383191dde2a7ae271f374793666b2778f765e65a
   return response.data.articles
 }
 
