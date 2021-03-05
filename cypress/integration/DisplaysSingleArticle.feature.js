43b9877cefcd545adf824fea6441cd3782fd6f37
Lucas Knudsen <laksteelhouse@gmail.com>
2021-03-05 10:07:27 +0100

    Tests the sad path

--- cypress/integration/DisplaysSingleArticle.feature.js
+++ cypress/integration/DisplaysSingleArticle.feature.js
@@ -1,12 +1,14 @@
 describe('client displays single article', () => {
-  describe('successfully with valid id', () => {
+  beforeEach(() => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3000/api/articles?article_type=story',
+      response: 'fixture:storiesList.json'
+    })
+  })
+  describe('successfully with valid article id', () => {
     beforeEach(() => {
-      cy.server()
-      cy.route({
-        method: 'GET',
-        url: 'http://localhost:3000/api/articles?article_type=story',
-        response: 'fixture:storiesList.json'
-      })
       cy.route({
         method: 'GET',
         url: 'http://localhost:3000/api/articles/**',
@@ -27,6 +29,28 @@ describe('client displays single article', () => {
       cy.get('[data-cy="article-body"]').should('contain', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui accumsan sit amet nulla facilisi. Fringilla est ullamcorper eget nulla facilisi etiam. Et tortor consequat id porta nibh venenatis cras sed felis. Arcu dui vivamus arcu felis. Vitae semper quis lectus nulla at. Neque vitae tempus quam pellentesque nec nam. Adipiscing at in tellus integer feugiat scelerisque varius morbi enim. A iaculis at erat pellentesque. Neque volutpat ac tincidunt vitae semper quis lectus nulla at.')
     })
 
+    it('sends user back to main view with logo click', () => {
+      cy.get('[data-cy="logo"]').click()
+      cy.url().should('be.equal', 'http://localhost:3001/')
+    })
+  })
+  describe('unsuccessfully with invalid article id', () => {
+    beforeEach(() => {
+      cy.route({
+        method: 'GET',
+        url: 'http://localhost:3000/api/articles/**',
+        response: {
+          message: "Article not found."
+        }, status: 404
+      })
+      cy.visit('/')
+      cy.visit('/articles/1337')
+    })
+
+    it('displays an error message', () => {
+      cy.get('[data-cy="not-found"]').should('contain', 'Article not found.')
+    })
+    
     it('sends user back to previous view with back button', () => {
       cy.get('[data-cy="back-button"]').click()
       cy.url().should('be.equal', 'http://localhost:3001/')
--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Header, Segment, Image } from 'semantic-ui-react'
+import { Container, Header, Segment, Button, Image } from 'semantic-ui-react'
 import { useParams, useHistory } from 'react-router-dom'
 import { getSingleArticle } from '../modules/articlesDataModule'
 import logo from '../assets/mainLogo.png'
@@ -7,29 +7,43 @@ import logo from '../assets/mainLogo.png'
 const SingleArticle = () => {
   const history = useHistory()
   const { id } = useParams()
-  const [content, setContent] = useState({})
+  const [content, setContent] = useState()
+  const [errorMessage, setErrorMessage] = useState()
 
   useEffect(() => {
     const fetchData = async () => {
-      let response = await getSingleArticle(id)
-      setContent(response)
+      try {
+        let response = await getSingleArticle(id)
+        setContent(response)
+      } catch (error) {
+        setErrorMessage(error.response ? error.response.data.message : error.message)
+      }
     }
     fetchData()
   }, [id])
 
   return (
     <Segment textAlign="center" padded>
-      <Image className="logo" as='a' alt="logo" src={logo} data-cy="back-button" onClick={() => history.goBack()} />
+      <Image className="logo" as='a' alt="logo" src={logo} href="/" data-cy="logo" />
       <Container textAlign="left" text>
-        <Segment padded >
-          <Header as="h1" data-cy="article-header" >
-            {content.title}
-            <Header.Subheader data-cy="article-meta">
-              Published: {content.date}
-            </Header.Subheader>
-            <Header as="h4" data-cy="article-teaser">{content.teaser}</Header>
-          </Header>
-          <p data-cy="article-body">{content.body}</p>
+        <Segment stacked padded >
+          {content ? (
+            <>
+              <Header as="h1" data-cy="article-header" >
+                {content.title}
+                <Header.Subheader data-cy="article-meta">
+                  Published: {content.date}
+                </Header.Subheader>
+                <Header as="h4" data-cy="article-teaser">{content.teaser}</Header>
+              </Header>
+              <p data-cy="article-body">{content.body}</p>
+            </>
+          ) : (
+              <>
+                <Header as="h2" data-cy="not-found">{errorMessage}</Header>
+                <Button color="teal" data-cy="back-button" onClick={() => history.goBack()}>Go back?</Button>
+              </>
+            )}
         </Segment>
       </Container >
     </Segment>
